4b50e1e3e74855c45cb33bbada4859b471fa8391
DAREKING2006 <93812857+DAREKING2006@users.noreply.github.com>
2021-12-25 21:52:34 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,26 @@
+
+import random
+import string
+
+print('hello welcome to a very new and advance password generator')
+
+
+length = int(input('\nEnter the length of password'))
+
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+
+all = lower + upper + num + symbols
+
+
+temp = random.sample(all,length )
+
+
+password = "".join(temp)
+
+
+print(password)
